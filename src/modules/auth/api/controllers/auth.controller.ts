e269eb6cdf9e007b1ded669b86f7f148d40bee37
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-21 13:19:31 +0300

    add: passwordRecovery to auth.controller.ts

--- src/modules/auth/api/controllers/auth.controller.ts
+++ src/modules/auth/api/controllers/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { SignUpDto } from '../../application/dto/signUp.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SignupCommand } from '../../application/use-cases/command/signup.command-handler';
+import { PasswordRecoveryDto } from '../../application/dto/password-recovery.dto';
+import { PasswordRecoveryCommand } from '../../application/use-cases/command/password-recovery.command-handler';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -12,5 +14,26 @@ export class AuthController {
   @Post('signup')
   async signUp(@Body() signUpDto: SignUpDto) {
     await this.commandBus.execute<SignupCommand, void>(new SignupCommand());
+  }
+
+  //Password recovery
+  @ApiOperation({ summary: 'Password recovery' })
+  @ApiResponse({
+    status: 204,
+    description: 'No content',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'If input data is incorrect',
+  })
+  @ApiBody({ type: PasswordRecoveryDto })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Post('password-recovery')
+  async passwordRecovery(@Body() passwordRecoveryDto: PasswordRecoveryDto) {
+    const res = await this.commandBus.execute<
+      PasswordRecoveryCommand,
+      Promise<boolean>
+    >(new PasswordRecoveryCommand(passwordRecoveryDto.email));
+    if (res) return;
   }
 }

a0ee00d784a27f33cf6de76b67adaa54d6fceb4c
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-22 18:02:55 +0300

    add: useGlobalPipes to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { swaggerSetup } from './core/adapters/swagger/swagger.setup';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,23 @@ async function bootstrap() {
     credentials: true,
     allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const errorsForResponse = [];
+        for (const e of errors) {
+          const key = Object.keys(e.constraints)[0];
+          errorsForResponse.push({
+            message: e.constraints[key],
+            field: e.property,
+          });
+        }
+        throw new BadRequestException(errorsForResponse);
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();

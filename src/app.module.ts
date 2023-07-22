161a8fa73835cfee5a113c1c38ad8473069cf7d9
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-22 17:57:17 +0300

    add: ThrottlerModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { getEnvConfig } from './core/common/config/env.config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -9,6 +10,10 @@ import { getEnvConfig } from './core/common/config/env.config';
       load: [getEnvConfig],
       envFilePath: ['.env'],
       isGlobal: true,
+    }),
+    ThrottlerModule.forRoot({
+      ttl: 10,
+      limit: 5,
     }),
     AuthModule,
   ],

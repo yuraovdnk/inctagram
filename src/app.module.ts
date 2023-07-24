283ecdb8e80d172994d65f45595af958a8d97b8a
Yurii Ovdiienko <yuraovdnk1999@ukr.net>
2023-07-24 12:58:36 +0300

    Revert "Password recovery (#4)"
    This reverts commit 17955528c9de3e6a5d6a9c05486b3beb2ffd705e.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { getEnvConfig } from './core/common/config/env.config';
-import { ThrottlerModule } from '@nestjs/throttler';
-
+//
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: [getEnvConfig],
-      envFilePath: ['.env'],
-      isGlobal: true,
-    }),
-    ThrottlerModule.forRoot({
-      ttl: 10,
-      limit: 5,
-    }),
-    AuthModule,
-  ],
+  imports: [AuthModule],
   controllers: [],
   providers: [],
 })
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,15 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './api/controllers/auth.controller';
-import { CqrsModule } from '@nestjs/cqrs';
-import { AuthRepository } from './infrastructure/repository/auth.repository';
-import { PasswordRecoveryCommandHandler } from './application/use-cases/command/password-recovery.command-handler';
-import { MailModule } from '../mailer/mail.module';
 
-const commandHandlers = [PasswordRecoveryCommandHandler];
-const queryHandlers = [];
-@Module({
-  imports: [CqrsModule, MailModule],
-  controllers: [AuthController],
-  providers: [...commandHandlers, ...queryHandlers, AuthRepository],
-})
+@Module({})
 export class AuthModule {}

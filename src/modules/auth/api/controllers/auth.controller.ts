8e13446d45110e7743a09aecf8d8650ff87a3449
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-26 23:01:38 +0300

    add: newPassword & logout routs

--- src/modules/auth/api/controllers/auth.controller.ts
+++ src/modules/auth/api/controllers/auth.controller.ts
@@ -11,17 +11,21 @@ import { SignUpDto } from '../../application/dto/request/signUp.dto';
 import {
   ApiBadRequestResponse,
   ApiBody,
+  ApiNoContentResponse,
   ApiOkResponse,
   ApiOperation,
-  ApiResponse,
   ApiTags,
+  ApiTooManyRequestsResponse,
+  ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { SignupCommand } from '../../application/use-cases/command/signup.command-handler';
 import { PasswordRecoveryDto } from '../../application/dto/request/password-recovery.dto';
 import { PasswordRecoveryCommand } from '../../application/use-cases/command/password-recovery.command-handler';
 import { ThrottlerGuard } from '@nestjs/throttler';
+import { NewPasswordDto } from '../../application/dto/request/new-password.dto';
+import { NewPasswordCommand } from '../../application/use-cases/command/new-password.command-handler';
 
-@ApiTags('auth')
+@ApiTags('AUTH')
 @Controller('auth')
 export class AuthController {
   constructor(private commandBus: CommandBus) {}
@@ -45,18 +49,18 @@ export class AuthController {
   }
 
   //Password recovery
-  @ApiOperation({ summary: 'Password recovery' })
-  @ApiResponse({
-    status: HttpStatus.NO_CONTENT,
+  @ApiOperation({
+    summary:
+      'Password recovery via Email confirmation. Email should be sent with RecoveryCode inside',
+  })
+  @ApiNoContentResponse({
     description:
       "Even if current email is not registered (for prevent user's email detection)",
   })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
+  @ApiBadRequestResponse({
     description: 'If the inputModel has invalid email',
   })
-  @ApiResponse({
-    status: HttpStatus.TOO_MANY_REQUESTS,
+  @ApiTooManyRequestsResponse({
     description: 'More than 5 attempts from one IP-address during 10 seconds',
   })
   @ApiBody({ type: PasswordRecoveryDto })
@@ -67,5 +71,45 @@ export class AuthController {
     await this.commandBus.execute<PasswordRecoveryCommand, Promise<boolean>>(
       new PasswordRecoveryCommand(passwordRecoveryDto.email),
     );
+  }
+
+  //New password
+  @ApiOperation({ summary: 'Confirm Password recovery' })
+  @ApiNoContentResponse({
+    description: 'If code is valid and new password is accepted',
+  })
+  @ApiBadRequestResponse({
+    description: 'If the inputModel has invalid email',
+  })
+  @ApiTooManyRequestsResponse({
+    description: 'More than 5 attempts from one IP-address during 10 seconds',
+  })
+  @ApiBody({ type: NewPasswordDto })
+  @UseGuards(ThrottlerGuard)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Post('new-password')
+  async newPassword(@Body() inputDto: NewPasswordDto) {
+    await this.commandBus.execute<NewPasswordCommand, Promise<boolean>>(
+      new NewPasswordCommand(inputDto.newPassword, inputDto.recoveryCode),
+    );
+  }
+
+  //Logout
+  @ApiOperation({
+    summary:
+      'In cookie client must send correct refreshToken that will be revoked',
+  })
+  @ApiNoContentResponse({
+    description:
+      'If the JWT refreshToken inside cookie is valid and user has been logged out',
+  })
+  @ApiUnauthorizedResponse({
+    description:
+      'If the JWT refreshToken inside cookie is missing, expired or incorrect',
+  })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Post('logout')
+  async logout() {
+    return;
   }
 }

af903248bff2d9a05b24ad386cf9ed8e65414b6f
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-26 23:06:18 +0300

    fix:   use crypto instead of UUID

--- src/modules/auth/domain/entity/password-recovery.entity.ts
+++ src/modules/auth/domain/entity/password-recovery.entity.ts
@@ -1,14 +1,15 @@
-import { v4 as uuidv4 } from 'uuid';
+import * as crypto from 'crypto';
 import { add } from 'date-fns';
 
 export class PasswordRecoveryEntity {
   code: string;
   expireAt: Date;
   userId: string;
+  createdAt: Date;
   static create(userId: string): PasswordRecoveryEntity {
     const entity = new PasswordRecoveryEntity();
     entity.userId = userId;
-    entity.code = uuidv4();
+    entity.code = crypto.webcrypto.randomUUID();
     entity.expireAt = add(new Date(), { hours: 1 });
     return entity;
   }

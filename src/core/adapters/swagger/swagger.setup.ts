2da6c6e898c1a9f07e162562ab4fec9be9ca7837
Yurii <yuraovdnk1999@ukr.net>
2023-08-02 21:42:52 +0300

    fix: swagger.setup.ts

--- src/core/adapters/swagger/swagger.setup.ts
+++ src/core/adapters/swagger/swagger.setup.ts
@@ -9,5 +9,5 @@ export function swaggerSetup(app: INestApplication) {
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 }

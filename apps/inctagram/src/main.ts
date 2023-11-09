4f95425adda6f3a1c16cdb89e205434e8eb9a56d
Yurii <yuraovdnk1999@ukr.net>
2023-11-09 19:59:08 +0200

    fix: bugs

--- apps/inctagram/src/main.ts
+++ apps/inctagram/src/main.ts
@@ -5,12 +5,15 @@ import { setupApp } from './setup-app';
 import { SwaggerConfig } from '../../../libs/adapters/api-doc/swagger.setup';
 import { WinstonModule } from 'nest-winston';
 import { winstonOptions } from '../../../libs/logger/winston-logger.config';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   let app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger(winstonOptions),
   });
   app.setGlobalPrefix('api/v1');
+  app.use(json({ limit: '20mb' }));
+  app.use(urlencoded({ limit: '20mb', extended: true }));
   app = setupApp(app);
   SwaggerConfig.setup(app);
 
